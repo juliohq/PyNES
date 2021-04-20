088ebed3e6f4af5e18b0153d77797d31d4f5450e
juliohq <JulioHenrique31501234@gmail.com>
2021-04-20 15:12:33 -0300

    Test raw status flags

--- tests/cpu_status_flags.py
+++ tests/cpu_status_flags.py
@@ -4,6 +4,9 @@ from PyNES import cpu
 def show():
 	print("C:", cpu.get_flag(cpu.C), "Z:", cpu.get_flag(cpu.Z), "I:", cpu.get_flag(cpu.I), "D:", cpu.get_flag(cpu.D), "B:", cpu.get_flag(cpu.B), "V:", cpu.get_flag(cpu.V), "N:", cpu.get_flag(cpu.N))
 
+def show_raw():
+	print(cpu.status)
+
 show()
 cpu.set_flag(cpu.C, 1)
 cpu.set_flag(cpu.Z, 1)
@@ -13,6 +16,7 @@ cpu.set_flag(cpu.B, 1)
 cpu.set_flag(cpu.V, 1)
 cpu.set_flag(cpu.N, 1)
 show()
+show_raw() # should print 127
 
 cpu.set_flag(cpu.C, 0)
 cpu.set_flag(cpu.Z, 0)
@@ -22,3 +26,4 @@ cpu.set_flag(cpu.B, 0)
 cpu.set_flag(cpu.V, 0)
 cpu.set_flag(cpu.N, 0)
 show()
+show_raw() # should print 0
