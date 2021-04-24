5ef56341d52c211a5062ba05f2d61efaa6ce87db
juliohq <JulioHenrique31501234@gmail.com>
2021-04-24 09:02:55 -0300

    Add RAM range test

--- tests/test_cpu_ram.py
+++ tests/test_cpu_ram.py
@@ -9,6 +9,9 @@ class test_cpu_ram(unittest.TestCase):
 		self.assertEqual(cpu.is_in_range(0x05), True)
 		self.assertEqual(cpu.is_in_range(-0x05), False)
 		self.assertEqual(cpu.is_in_range(0x801), False)
+		self.assertEqual(cpu.is_in_range(0x7FF), True)
+		self.assertEqual(cpu.is_in_range(0x800), False)
+		self.assertEqual(cpu.is_in_range(0x00), True)
 
 if __name__ == "__main__":
 	unittest.main()
