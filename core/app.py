41f34676905f18366c10861e7f1ba7a4e8a53015
juliohq <JulioHenrique31501234@gmail.com>
2021-05-31 08:50:17 -0300

    Add DebugScreen

--- core/app.py
+++ core/app.py
@@ -1,12 +1,34 @@
 from core.nes import cartridge, cpu
 from core.engine import graphics
+from math import floor
+import pygame
+
+is_debug = True
+
+# pygame.init()
+clock = pygame.time.Clock()
+
+clocks = 0
 
 def run():
-	window = graphics.Window()
+	if is_debug:
+		window = graphics.Window((512, 240))
+		debug = graphics.DebugScreen(window.screen)
+	else:
+		window = graphics.Window((256, 240))
 	
 	# Game loop
 	while True:
 		window.draw()
 		window.update()
+		
+		if is_debug:
+			global clocks
+			if clocks % 3 == 0:
+				pygame.display.set_caption("PyNES (FPS: %s | %s)" % (floor(clock.get_fps()), clocks))
+			debug.draw()
+			clocks += 1
+		
+		clock.tick(60)
 	
 	graphics.pygame.quit()
--- core/engine/graphics.py
+++ core/engine/graphics.py
@@ -2,17 +2,17 @@ import pygame
 import sys
 
 class Window:
-	def __init__(self):
+	def __init__(self, size):
 		pygame.init()
 		
-		self.size = (256, 240)
+		self.size = size
 		
 		self.width = self.size[0]
 		self.height = self.size[1]
 		
 		self.black = (0, 0, 0)
 		
-		self.screen = pygame.display.set_mode(self.size)
+		self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE | pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.SCALED)
 		pygame.display.set_caption("PyNES")
 	
 	def update(self):
@@ -21,5 +21,15 @@ class Window:
 				sys.exit()
 	
 	def draw(self):
-		self.screen.fill(self.black)
+		# self.screen.fill(self.black)
+		# pygame.display.update()
 		pygame.display.flip()
+
+class DebugScreen:
+	def __init__(self, surface):
+		self.surface = surface
+	
+	def draw(self):
+		surface = pygame.Surface((256, 240))
+		pygame.draw.rect(surface, (0, 0, 255), pygame.Rect(0, 0, 256, 240))
+		self.surface.blit(surface, (256, 0))
