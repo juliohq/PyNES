7754b0642da80c914e18eb8b1e6f903bf301ea50
juliohq <juliohenrique31501234@gmail.com>
2021-08-20 17:21:03 -0300

    change to QThread, add widgets

--- core/app.py
+++ core/app.py
@@ -1,29 +1,59 @@
 import sys
 from core.engine import settings
 from core.engine.worker import Worker
-from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout
-from PyQt5.QtCore import QThreadPool
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication, QLabel, QPlainTextEdit, QPushButton, QWidget, QVBoxLayout
 
 is_debug = settings.config.getboolean('main', 'debug')
 
-paused = False
+# core
+thread = QThread()
+worker = Worker()
+
+# gui
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('PyNES Debugger')
+layout = QVBoxLayout()
+window.setLayout(layout)
+
+# create widgets
+engine_clocks = QLabel()
+cpu_clocks = QLabel()
+fps = QLabel()
+instruction = QLabel()
+
+dump = QPlainTextEdit()
+dump.setReadOnly(True)
+
+# add widgets to layout
+layout.addWidget(engine_clocks)
+layout.addWidget(cpu_clocks)
+layout.addWidget(fps)
+layout.addWidget(instruction)
+layout.addWidget(dump)
 
 if '-d' in sys.argv or '--debug' in sys.argv or is_debug:
 	print('DEBUG MODE')
 	is_debug = True
 
+def update(ec, cc, efps, ins):
+	engine_clocks.setText(f'Engine Clocks: {ec}')
+	cpu_clocks.setText(f'CPU Clocks: {cc}')
+	fps.setText(f'FPS: {efps}')
+	instruction.setText(f'Current Instruction: {ins}')
+
+def quit():
+	sys.exit()
+
 def run():
-	worker = Worker()
-	
 	if is_debug:
-		pool = QThreadPool()
-		pool.start(worker)
+		worker.moveToThread(thread)
+		thread.started.connect(worker.run)
+		worker.update.connect(update)
+		worker.quit.connect(quit)
+		thread.start()
 		
-		app = QApplication([])
-		window = QWidget()
-		layout = QVBoxLayout()
-		window.setLayout(layout)
-		layout.addWidget(QPushButton("Button"))
 		window.show()
 		app.exec()
 	else:
--- core/engine/worker.py
+++ core/engine/worker.py
@@ -1,30 +1,51 @@
-import pygame, sys
+import pygame
 from pygame.locals import *
-from PyQt5.QtCore import QRunnable
+from PyQt5.QtCore import pyqtSignal, QObject
+from core.nes import cpu
 
-class Worker(QRunnable):
+class Worker(QObject):
+	update = pyqtSignal(int, int, float, str)
+	clock = pyqtSignal()
+	quit = pyqtSignal()
+	
 	def run(self):
-		print("Worker started!")
+		print('Worker started!')
 		
+		self.is_running = True
 		pygame.init()
-		clock = pygame.time.Clock()
-		clocks = 0
-		paused = False
-		screen = pygame.display.set_mode((256, 240))
+		self.clock = pygame.time.Clock()
+		self.clocks = 0
+		self.paused = False
+		pygame.display.set_caption('PyNES')
+		self.screen = pygame.display.set_mode((256, 240))
 		
-		while True:
+		while self.is_running:
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
 					if event.key == K_p:
 						paused = not paused
 					elif event.key == K_ESCAPE:
+						self.is_running = False
+						self.quit.emit()
 						pygame.quit()
-						sys.exit()
 				elif event.type == QUIT:
+					self.is_running = False
+					self.quit.emit()
 					pygame.quit()
-					sys.exit()
 			
-			if not paused:
-				if True:
-					clocks += 1
-			clock.tick(60)
+			if not self.is_running:
+				break
+			
+			# update
+			self.update.emit(
+				self.clocks,            # Engine Clocks
+				cpu.clock_count,        # CPU Clocks
+				self.clock.get_fps(),   # FPS
+				cpu.lookup[cpu.op][0], 	# Instruction
+			)
+			
+			if not self.paused:
+				self.clocks += 1
+			self.clock.tick(60)
+			
+			pygame.display.flip()
